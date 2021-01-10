d1d1a576959763c4c6120f469fef2cfe40cf6dfc
Gaurav Kumar <dhimangaurav2050@gmail.com>
2021-01-11 00:17:03 +0530

    Text on Video

--- add_text_to_videos.py
+++ add_text_to_videos.py
@@ -57,5 +57,5 @@ cv2.destroyAllWindows()
 
 
 
-# video capture properties
+#  video capture properties
 # https://docs.opencv.org/4.0.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
